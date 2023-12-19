6cbd156964f2da5685290969673bf68e9d6935fc
def1nt <def1nt@mail.ru>
2023-12-19 15:05:29 +0300

    fix: changed paths in settings for now

--- AppSettings.cs
+++ AppSettings.cs
@@ -1,6 +1,6 @@
-static class AppSettings
+static class AppSettings // TODO: Discover these from environment variables
 {
-    internal static readonly string VictoriaMetricsURL = "http://192.168.105.12:4242/api/put";
-    internal static readonly string PostgresConnectionString = "Host=192.168.105.12;Username=postgres;Password=sqladmin;Database=cloud_vmz";
+    internal static readonly string VictoriaMetricsURL = "http://192.168.105.35:4242/api/put";
+    internal static readonly string PostgresConnectionString = "Host=192.168.105.35:5433;Username=postgres;Password=mysimplepassword;Database=vmz_cloud";
     internal static readonly System.Net.IPAddress ListenOn = System.Net.IPAddress.Any;
 }
