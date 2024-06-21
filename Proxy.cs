4cf37cf7d46e697290b04dc10902b285dd882943
def1nt <def1nt@mail.ru>
2024-06-21 14:14:58 +0300

    feat: kinda functional proxy-bundler-cache

---
+++ Proxy.cs
@@ -0,0 +1,202 @@
+using System.Text.Json;
+using Modbus;
+using Utils;
+using System.Net.Sockets;
+
+public sealed class PollerProxy
+{
+    MachineParameters? machineParameters;
+
+    List<(int addr, byte code)> addresses = new(); // Assume sorted
+    List<Bundle> addressBundles = new();
+    private readonly NetworkStream _stream;
+
+    Dictionary<(int addr, byte code), ushort> data = new();
+
+    public PollerProxy(MachineParameters? machineParameters, NetworkStream stream)
+    {
+        this.machineParameters = machineParameters ?? new MachineParameters(0, 0);
+        this._stream = stream;
+        ExtractAddresses();
+        Bundle(1);
+        Bundle(3);
+        ExtendBundles();
+    }
+
+    private void ExtractAddresses()
+    {
+        if (machineParameters == null) return;
+
+        foreach (var parameter in machineParameters.Parameters)
+        {
+            (int, byte) addr = (StringUtils.HexStringToUShort(parameter.Address), parameter.Function);
+            addresses.Add(addr);
+        }
+        addresses = addresses.OrderBy(a => a.addr).ToList();
+    }
+
+    public void Bundle(int f)
+    {
+        int previous = addresses[0].addr - 1;
+        Bundle newb = new Bundle
+        {
+            functionCode = f
+        };
+        addressBundles.Add(newb);
+
+        foreach (var address in addresses.Where(a => a.code == f))
+        {
+            if (address.addr - previous <= 4)
+            {
+                addressBundles[^1].Add(address.addr);
+            }
+            else if (address.addr > previous + 1)
+            {
+                newb = new Bundle() { address.addr };
+                newb.functionCode = f;
+                addressBundles.Add(newb);
+            }
+            previous = address.addr;
+        }
+        // System.Console.WriteLine(addressBundles.Count);
+    }
+
+    private void ExtendBundles()
+    {
+        foreach (var bundle in addressBundles)
+        {
+            System.Console.WriteLine(bundle.Count);
+            if (bundle.Count < 1) continue;
+            int first = bundle[0];
+            int last = bundle[^1];
+
+            int lastLenght = StringUtils.DecodeTypeFromString(machineParameters!.Parameters.Find(x => StringUtils.HexStringToUShort(x.Address) == last)?.Type ?? "").length;
+
+            int length = last - first + lastLenght;
+            bundle.Length = length;
+        }
+    }
+
+    public async Task PollBundle(Bundle bundle)
+    {
+        // Goind through bundles, querying data from first address with length of bundle
+        // Receiving data and putting each int16 into a data dictionary with address as key
+        int first = bundle[0];
+        byte code = (byte)bundle.functionCode;
+        RequestPacket request = new(1, code, (ushort)first, (ushort)bundle.Length);
+        var result = await SendReceiveAsync(request, 3);
+        if (result.Data.Length == 0)
+        {
+            throw new Exception("No data received");
+        }
+        for (int i = 0; i < result.Data.Length; i++)
+        {
+            if (data.ContainsKey((first + i, code))) data[(first + i, code)] = result.Data[i];
+            else data.Add((first + i, code), result.Data[i]);
+        }
+        bundle.Stale = false;
+        bundle.lastUpdate = DateTime.Now;
+    }
+
+    public ushort[] GetData(RegisterInfo registerInfo)
+    {
+        // Looking to which bundle the address belongs
+        Bundle? bundle = addressBundles.Find(x => x.Contains(StringUtils.HexStringToUShort(registerInfo.Address)) && x.functionCode == registerInfo.Function);
+        if (bundle is null)
+        {
+            throw new Exception($"Address not found in any bundle: {registerInfo.Address}");
+        }
+        else if (bundle.Stale)
+        {
+            // Console.WriteLine("Started polling");
+            PollBundle(bundle).Wait();
+            // Console.WriteLine("Finished polling");
+        }
+
+        // Everything is fine, look for data and return it
+        var size = StringUtils.DecodeTypeFromString(registerInfo.Type).length;
+        var first = StringUtils.HexStringToUShort(registerInfo.Address);
+        byte code = registerInfo.Function;
+        ushort[] data = new ushort[size];
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = this.data[(first + i, code)];
+        }
+
+        return data;
+    }
+
+    private async Task<ResponsePacket> SendReceiveAsync(RequestPacket request, int retries = 3)
+    {
+        if (retries < 0)
+            throw new TimeoutException($"Modbus request timed out: {request.FunctionCode} at {request.Address}");
+
+        byte[] buffer = new byte[256];
+        int bytesRead;
+        CancellationTokenSource cts = new(TimeSpan.FromMilliseconds(2000));
+        try
+        {
+            await _stream.WriteAsync(request.Data, cts.Token);
+            bytesRead = await _stream.ReadAsync(buffer.AsMemory(0, 256), cts.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            return await SendReceiveAsync(request, retries - 1);
+        }
+        ResponsePacket response = new();
+        if (bytesRead != 0)
+        {
+            // trying to detect if the packet was duplicated because of previous read timeout
+            if (bytesRead % 2 == 0 && buffer.Take(bytesRead / 2).SequenceEqual(buffer.Skip(bytesRead / 2).Take(bytesRead / 2)))
+            {
+                bytesRead /= 2;
+            }
+            response.SetData(buffer.Take(bytesRead).ToArray());
+            if (response.ExceptionCode != 0)
+                throw new Exception($"Modbus exception code: {response.ExceptionCode} {request.FunctionCode} at {request.Address}");
+        }
+        return response;
+    }
+
+    public override string ToString()
+    {
+        // return JsonSerializer.Serialize(addresses) + "\n" + JsonSerializer.Serialize(addressBundles, new JsonSerializerOptions() { IncludeFields = true, MaxDepth = 2 }) + $"\nOriginally addresses: {addresses.Count}; After bundling bundles: {addressBundles.Count}\n";
+        return JsonSerializer.Serialize(addresses) + "\n" + addressBundles.Aggregate("", (b, s) => s + b.ToString()) + $"\nOriginally addresses: {addresses.Count}; After bundling bundles: {addressBundles.Count}\n";
+    }
+}
+
+public class Bundle : List<int>
+{
+    public int functionCode = 3;
+    public int Length = 0;
+    public bool Stale
+    {
+        get
+        {
+            if (stale) return true;
+            else
+            {
+                if ((DateTime.Now - lastUpdate) > TimeSpan.FromSeconds(10))
+                {
+                    return stale = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        set
+        {
+            stale = value;
+        }
+    }
+    private bool stale = true;
+    public DateTime lastUpdate;
+    public Bundle() : base() { }
+
+    public override string ToString()
+    {
+        return $"Bundle: {JsonSerializer.Serialize(this)},\tLength: {Length}\n";
+    }
+}
